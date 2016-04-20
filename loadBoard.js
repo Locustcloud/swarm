4fca2455e19c01c85b0c151ea7d21ab1baa87c01
Locustcloud <leon.marbl@gmail.com>
2016-04-20 17:46:43 +0200

    Index filer og stylesheet
    Mer er på vei, men jeg prøver å sorte ut arrayen med punktene jeg skal flytte spiller markøren til.

--- loadBoard.js
+++ loadBoard.js
@@ -3,5 +3,5 @@ window.onload = function() {
 	var c = document.getElementById("mainBoard");
 	var ctx = c.getContext("2d");
 	var img = document.getElementById("brett");
-	ctx.drawImage(img, 1, 1, 1280,645);
-}
+	ctx.drawImage(img, 0, 0, 1280,645);
+};
