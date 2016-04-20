4ac42d0364fbe65b0603afa365523d367fd8d688
leonm11 <leonm11@student.uia.no>
2016-04-20 21:39:35 +0200

    Update200416 - Meeple flyttet inn i loadBoard.js

--- loadBoard.js
+++ loadBoard.js
@@ -3,5 +3,7 @@ window.onload = function() {
 	var c = document.getElementById("mainBoard");
 	var ctx = c.getContext("2d");
 	var img = document.getElementById("brett");
+	var imgMeep = document.getElementById("meeple");
 	ctx.drawImage(img, 0, 0, 1280,645);
+	ctx.drawImage(imgMeep, 1208, 559, 40, 40);
 };
