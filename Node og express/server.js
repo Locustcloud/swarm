2030b0828355f4020eb857c5d9ec110171c400e1
leonm11 <leonm11@student.uia.no>
2016-04-27 03:41:58 +0200

    Fullført chat funksjon
    Vel, kanskje ikke helt. Nå kan en mengde spillere i det minste snakke
    sammen. En ting som jeg vil tenke på å legge til senere er å limite
    mengden brukere til 4, og når et nytt game starter at en ny game session
    opprettes. Jeg er trøtt x) Natta.

--- Node og express/server.js
+++ Node og express/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients = {};
 
 app.use(express.static('mm502'));
 
@@ -10,15 +11,20 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function(socket, msg) {
-	console.log('a user connected');
+	console.log('User connected:', socket.id);
 	io.emit('User connected', msg);
 	
 	socket.on('disconnect', function(){
-		console.log('a disconnected');
+		console.log('a user disconnected');
 	});
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		io.emit('chat message', clients[socket.id].username + ': ' + msg);
+	});
+	socket.on('user name', function(user) {
+		console.log('username: ' + user);
+		clients[socket.id] = {'username':user};
+		console.log(clients[socket.id].username);
 	});
 });
 
