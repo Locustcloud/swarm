815873d98acadd55c6087b4a2f09444fa1319260
leonm11 <leonm11@student.uia.no>
2016-05-04 11:22:24 +0200

    4 player mode
    Lagt til at spillet ikke fortsetter før alle 4 spillere er klare. Skal
    legge til en fillText som sier "Waiting for other players".

--- Node og express/server.js
+++ Node og express/server.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var clients = {};
+var clientsReady = {};
 
 app.use(express.static('mm502'));
 
@@ -11,20 +12,37 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function(socket, msg) {
-	console.log('User connected:', socket.id);
-	io.emit('User connected', msg);
+	console.log('user connected:', socket.id);
+	console.log(Object.keys(io.engine.clients).length);
 	
+	if (Object.keys(io.engine.clients).length > 4) {
+		//io.sockets.socket(socket.id).emit("Serverfull");
+		socket.disconnect();
+		console.log('a user disconnected');
+	}
+
 	socket.on('disconnect', function(){
 		console.log('a user disconnected');
 	});
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-		io.emit('chat message', clients[socket.id].username + ': ' + msg);
+		io.emit('chat message', '<strong>' + clients[socket.id].username + '</strong>' + ': ' + msg);
 	});
 	socket.on('user name', function(user) {
 		console.log('username: ' + user);
 		clients[socket.id] = {'username':user};
-		console.log(clients[socket.id].username);
+		socket.broadcast.emit('user connected', user);
+	});
+	
+	socket.on('introDialogFinnished', function() {
+		clientsReady[socket.id] = true;
+		console.log(clients[socket.id].username + ' Is ready to rumble');
+		console.log(Object.keys(clientsReady).length);
+		if (Object.keys(clientsReady).length > 3) {
+			io.emit('readyToRumble', "Ready");
+			socket.broadcast.emit('readyToRumble', "Ready");
+			console.log('Everybody ready to rumble')
+		}
 	});
 });
 
--- loadBoard.js
+++ loadBoard.js
@@ -1,16 +1,34 @@
 // Global vars
-var imgBoard = new Image();
+var indexDialog = 0;
+var imgDialog = new Image();
+var backDrop = new Image();
+//var imgBoard = new Image();
 var imgMeep = new Image();
 var canvas = document.getElementById("mainBoard");
 var ctx = canvas.getContext("2d");
-var meepX = 1208;
-var meepY = 559;
+var StartAnimation = true;
 
-/*function init() {
+var textDialogs = [
+	"Hello, and welcome to Parthenon. The glorious thing that is this.. thingoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+	"This is a test nr. 2",
+];
+
+function init() {
 	
-	imgBoard.src = 'img/brett_border.jpg';
-	imgMeep.src = 'img/meeple.png';
-	window.requestAnimationFrame(draw);
+	imgDialog.src = 'img/text_area.png';
+	backDrop.src = 'img/skau.png';
+	//imgMeep.src = 'img/meeple.png';
+	window.requestAnimationFrame(welcomeText);
+}
+
+function ctrlBtn() {
+	indexDialog = indexDialog + 1; 
+	if (indexDialog < textDialogs.length)
+		document.getElementById("storyDialog").value = textDialogs[indexDialog];
+	else { 
+		document.getElementById("storyDialog").value = "";
+		socket.emit('introDialogFinnished');
+	}
 }
 
 function moveMeep() {
@@ -19,17 +37,43 @@ function moveMeep() {
 }
 
 function draw() {
-	ctx.clearRect(0,0,1280,645); // clear canvas
+	ctx.clearRect(0,0,960,720); // clear canvas
 	
-	ctx.drawImage(imgBoard, 0,0,1280,645);
+	//tegner første tekstbox
+	ctx.drawImage(imgDialog, 18, 550, 926, 130);
+	ctx.drawImage(backDrop, -100, -100, 1440, 900);
 	ctx.save();
+	/*ctx.font = "14px Arial"
+	ctx.fillStyle = "rgba(255,255,255,1)";
+	ctx.fillText("Hello, and welcome to Parthenon. The glorious thing that is this.. thing", 70, 580);*/
 	
-	//Meep
-	ctx.drawImage(imgMeep, meepX, meepY, 40, 40);
+	/*//Meep
+	ctx.drawImage(imgMeep, meepX, meepY, 40, 40);*/
 	ctx.restore();
 	
 	window.requestAnimationFrame(draw);
 	
 }
 
-init();*/
+function welcomeText() {
+	ctx.clearRect(0,0,960,720); // clear canvas
+	
+	//tegner første tekstbox
+	ctx.drawImage(imgDialog, 18, 550, 926, 130);
+	ctx.save();
+	
+	/*ctx.font = "14px Arial"
+	ctx.fillStyle = "rgba(255,255,255,1)";
+	ctx.fillText(textDialogs[indexDialog], 70, 580);*/
+	
+	/*//Meep
+	ctx.drawImage(imgMeep, meepX, meepY, 40, 40);*/
+	ctx.restore();
+	if (StartAnimation) 
+		window.requestAnimationFrame(welcomeText);
+	else
+		ctx.clearRect(0,0,960,720); // clear canvas
+	
+}
+
+init();
