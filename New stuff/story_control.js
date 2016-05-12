8406412f9bad24b0bc332f81b6125b53394020fa
leonm11 <leonm11@student.uia.no>
2016-05-12 13:29:26 +0200

    Commit12052016_1

--- New stuff/story_control.js
+++ New stuff/story_control.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+var inv = ["Bronsesverd"," Bronserustning"," kart"];
+
+function listPackage() {
+	"use strict";
+	document.getElementById("inventory").innerHTML = "<strong>Innholdet i gaven: </strong>" + inv;
+}
+
+function listInv() {
+	"use strict";
+	document.getElementById("inventory").innerHTML = "<strong>Inventar: </strong>" + inv;
+}
+
+
+
+/*$(document).ready(function() {
+	"use strict";
+    $('.contBtn').click(scene1);
+});
+
+function scene1() {
+	"use strict";
+	$('#begynnelsen').replaceWith($('#begynnelsen2'));
+	$('#UI_start').replaceWith($('#UI_1'));
+	$('#scene1').show();*/
