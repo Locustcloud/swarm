7904e635775f9567ac9ad58a8dfa9783b80bb695
leonm11 <leonm11@student.uia.no>
2016-05-15 13:27:22 +0200

    Commit
    La til bedre bag script og kontroll. Jobber med timed events.

--- New stuff/story_control.js
+++ New stuff/story_control.js
@@ -9,10 +9,12 @@ function ShowThisShit(){
 	  $(".inventory").append("<ul id='list'><b><u>Inventar: <br><u></b></ul>");
 	  $.each(inv, function(n, elem) {
 	  $("#list").append("<li>" + elem + "</li>");
+	  $(".inventory").show();
 	  console.log(executed);
 	  });
   } else{
 	  $("#list").remove();
+	  $(".inventory").hide();
 	  executed = false;
  }
 }
@@ -24,13 +26,13 @@ function listPackage() {
 
 function herbs() {
 	"use strict";
-	document.getElementById('valg3_9').scrollIntoView();
+	document.getElementById('scene12_gutt_tempel').scrollIntoView();
 	document.getElementById("getHerb").innerHTML = "<strong>En prest kommer bort til deg og gir deg en velsignet skriftrull med legende urter i seg.</strong>";
 	inv.push(" urter");
 }
 
 function showBar() {
 	"use strict";
-	document.getElementById('valg9_gutt').scrollIntoView();
-	document.getElementById("inventory").style.visibility = "visible";
+	document.getElementById('scene10_gutt').scrollIntoView();
+	document.getElementById("bag").style.visibility = "visible";
 }
